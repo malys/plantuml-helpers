47626042bf53f8ef5ac84edc410211bac5b0822c
Malys <malys@mageos.com>
2024-05-27 10:02:31 +0200

    fix: bandizip cli name

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ ZIP=vscode-helpers.zip
 URL=$(curl -s https://api.github.com/repos/malys/plantuml-helpers/releases/latest | grep "browser_download_url" | cut -d : -f 2,3 | tr -d \" )
 echo Downloading $URL
 curl -qLk $URL -o $ZIP
-"/c/Program Files/Bandizip/bc.exe"  x -target:auto -o:./release -y $ZIP
+"/c/Program Files/Bandizip/bz.exe"  x -target:auto -o:./release -y $ZIP
 rm $ZIP
 
 SOURCE_PATH=B:\\prog\\code\\plantuml-helpers\\release\\vscode-helpers\\
